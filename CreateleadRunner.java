74d4605416310352d31438954f6571ef1970052a
lokeshbabu17 <93443321+lokeshbabu17@users.noreply.github.com>
2021-12-20 16:53:11 +0530

    Delete CreateleadRunner.java

--- CreateleadRunner.java
+++
@@ -1,9 +0,0 @@
-package week6.day1;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(features="src/main/java/features/createLead.feature",glue="stepdefinition",monochrome=true,publish=true)
-
-public class CreateleadRunner extends AbstractTestNGCucumberTests {
-
-}
